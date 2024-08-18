207b8df6a80aac8a227bdde23a29e2519e6369c2
Bhanu Pratap Shukla <107072461+bhanushuklaa@users.noreply.github.com>
2024-08-18 11:51:54 +0530

    commit 6

--- Portfolio/assets/js/google-maps.js
+++ Portfolio/assets/js/google-maps.js
@@ -1,6 +1,6 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: -6.207690, lng: 106.985270};
+    var myLatLng = { lat: 25.966958492370626, lng: 83.79573315730597 };
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
         zoom: 17,
@@ -10,6 +10,6 @@ function initMap() {
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: 'South Jakarta, INA' // Title Location
+        title: 'Nikasi, Ballia' // Title Location
     });
 }
